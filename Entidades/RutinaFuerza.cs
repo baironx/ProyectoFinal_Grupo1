e6cc500e35ab3af47228e4e523f534fe76094227
jonathancha94 <jonathanchavarria94@gmail.com>
2025-08-01 22:36:36 -0600

    Update RutinaFuerza.cs

--- Entidades/RutinaFuerza.cs
+++ Entidades/RutinaFuerza.cs
@@ -10,3 +10,26 @@ namespace ProyectoFinal_Grupo1_AppEntrenamiento.Entidades
     {
     }
 }
+
+
+
+namespace AppEntrenamientoPersonal.Entidades
+{
+    /// <summary>
+    /// Clase que representa una rutina de entrenamiento de fuerza.
+    /// Hereda de la clase Rutina.
+    /// </summary>
+    public class RutinaFuerza : Rutina
+    {
+        public RutinaFuerza(int duracion, string intensidad, string grupoMuscular, string nombreAtleta, DateTime fechaRealizacion, DateTime? fechaVencimiento, string lesionesPostEntrenamiento)
+            : base("Fuerza", duracion, intensidad, grupoMuscular, nombreAtleta, fechaRealizacion, fechaVencimiento, lesionesPostEntrenamiento) { }
+ 
+        /// <summary>
+        /// Implementación específica de cómo se describe una rutina de fuerza.
+        /// Sobrescribe el método abstracto de la clase base.
+        /// </summary>
+        public override string Describir()
+        {
+            return $"[Fuerza] {Duracion} min - Intensidad: {Intensidad} - Grupo: {GrupoMuscular} - Realizada: {FechaRealizacion.ToShortDateString()}";
+        }
+    }
