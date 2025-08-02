5d1674be1de188470105bf57520f2b29b14b0d02
jonathancha94 <jonathanchavarria94@gmail.com>
2025-08-01 22:35:03 -0600

    Update Atleta.cs

--- Entidades/Atleta.cs
+++ Entidades/Atleta.cs
@@ -12,3 +12,63 @@ namespace AppEntrenamientoPersonal.Entidade
     }
 
 }
+
+
+
+namespace AppEntrenamientoPersonal.Entidades
+
+{
+
+    public interface IAtleta
+
+    {
+
+        string Nombre { get; }
+
+        double Peso { get; }
+
+        double Altura { get; }
+
+        string Objetivos { get; }
+
+        string Nivel { get; }
+
+        string ToString();
+
+
+
+    }
+
+    public class Atleta : IAtleta
+
+    {
+
+        public string Nombre { get; set; }
+
+        public double Peso { get; set; }
+
+        public double Altura { get; set; }
+
+        public string Objetivos { get; set; }
+
+        public string Nivel { get; set; }
+
+        public override string ToString()
+
+        {
+
+            return $"Atleta: {Nombre}\n" +
+
+                   $"Peso: {Peso} kg\n" +
+
+                   $"Altura: {Altura} m\n" +
+
+                   $"Objetivos: {Objetivos}\n" +
+
+                   $"Nivel: {Nivel}";
+
+        }
+
+    }
+
+}
