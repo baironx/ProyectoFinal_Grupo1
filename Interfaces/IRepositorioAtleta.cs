0d4964a9303b0232f27a435d506199d3874881dc
jonathancha94 <jonathanchavarria94@gmail.com>
2025-08-01 22:19:39 -0600

    Update IRepositorioAtleta.cs

--- Interfaces/IRepositorioAtleta.cs
+++ Interfaces/IRepositorioAtleta.cs
@@ -10,3 +10,18 @@ namespace ProyectoFinal_Grupo1_AppEntrenamiento.Interfaces
     {
     }
 }
+
+
+using System.Collections.Generic;
+using AppEntrenamientoPersonal.Entidades;
+
+namespace AppEntrenamientoPersonal.Interfaces
+{
+    public interface IRepositorioAtleta
+    {
+        void Agregar(Atleta atleta);
+        Atleta BuscarPorNombre(string nombre);
+        IEnumerable<Atleta> ObtenerTodos();
+    }
+}
+
